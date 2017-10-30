ca67c5a145866d69c11e4487ed18733d6a4b8368
Jonathan Yiv <JonathanYiv@gmail.com>
2017-10-30 14:31:44 -0400

    integrated simply with flickr api

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    search = params[:username]
+    search = "153671534@N08" if search.nil? || search.empty?
+    @photos = flickr.photos.search(user_id: search, extras: "url_o")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
+  get '/search', to: 'static_pages#home'
 end
